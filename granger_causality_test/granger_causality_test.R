627eb8344e8382f28b384107d2f0e357ad98fe7b
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-03-17 07:30:10 +0700

    granger_causality_test (grangersearch) R Software

---
+++ granger_causality_test/granger_causality_test.R
@@ -0,0 +1,17 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Perform a Granger Causality Test on Two Time Series Use granger_causality_test (grangersearch) With (In) R Software
+install.packages("caustests")
+library("caustests")
+# Estimation Perform a Granger Causality Test on Two Time Series Use granger_causality_test (grangersearch) With (In) R Software
+granger_causality_test = read.csv("https://raw.githubusercontent.com/timbulwidodostp/granger_causality_test/main/granger_causality_test/granger_causality_test.csv",sep = ";")
+x <- granger_causality_test$Independen
+y <- granger_causality_test$Dependen
+granger_causality_test <- granger_causality_test(x = x, y = y)
+summary(granger_causality_test)
+# Perform a Granger Causality Test on Two Time Series Use granger_causality_test (grangersearch) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
